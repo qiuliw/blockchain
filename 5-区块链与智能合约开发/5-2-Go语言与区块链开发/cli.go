b760367e6af90374711df769df578ec94d4adea7
qiuliw <a1807191473@gmail.com>
2026-06-09 07:20:51 +0800

    add: send 命令

--- 5-区块链与智能合约开发/5-2-Go语言与区块链开发/cli.go
+++ 5-区块链与智能合约开发/5-2-Go语言与区块链开发/cli.go
@@ -11,10 +11,13 @@ type CLI struct {
 }
 
 const Usage = `
-addBlock --data DATA              "add data to blockchain"
-printChain                        "print all blockchain data"
-getBalance --address ADDRESS      "get blockchain address balance"
+addBlock --data DATA                                              "add data to blockchain"
+printChain                                                        "print all blockchain data"
+getBalance --address ADDRESS                                      "get blockchain address balance"
+send --from FROM --to TO --amount AMOUNT --miner MINER [--data]   "send coin from one address to another"
 `
+
+// send [--data] coinbase 附带数据
 
 // Run 入口
 func (cli *CLI) Run() {
@@ -27,6 +30,13 @@ func (cli *CLI) Run() {
 	addBlockCmd := flag.NewFlagSet("addBlock", flag.ExitOnError)
 	printChainCmd := flag.NewFlagSet("printChain", flag.ExitOnError)
 	getBalanceCmd := flag.NewFlagSet("getBalance", flag.ExitOnError)
+	sendCmd := flag.NewFlagSet("send", flag.ExitOnError)
+
+	sendFrom := sendCmd.String("from", "", "source address")
+	sendTo := sendCmd.String("to", "", "destination address")
+	sendAmount := sendCmd.Int64("amount", 0, "transfer amount")
+	sendMiner := sendCmd.String("miner", "", "miner address")
+	sendData := sendCmd.String("data", "", "miner data")
 
 	// 参数定义
 	addBlockData := addBlockCmd.String("data", "", "block data")
@@ -52,14 +62,18 @@ func (cli *CLI) Run() {
 			panic(err)
 		}
 
+	case "send":
+		err := sendCmd.Parse(os.Args[2:])
+		if err != nil {
+			panic(err)
+		}
+
 	default:
 		fmt.Print(Usage)
 		os.Exit(1)
 	}
 
-	// =========================
 	// addBlock
-	// =========================
 	if addBlockCmd.Parsed() {
 
 		if *addBlockData == "" {
@@ -75,16 +89,12 @@ func (cli *CLI) Run() {
 		fmt.Println("add block success")
 	}
 
-	// =========================
 	// printChain
-	// =========================
 	if printChainCmd.Parsed() {
 		cli.bc.PrintChain()
 	}
 
-	// =========================
 	// getBalance
-	// =========================
 	if getBalanceCmd.Parsed() {
 
 		if *getBalanceAddress == "" {
@@ -93,6 +103,26 @@ func (cli *CLI) Run() {
 		}
 
 		cli.GetBalance(*getBalanceAddress)
+	}
+
+	if sendCmd.Parsed() {
+
+		if *sendFrom == "" ||
+			*sendTo == "" ||
+			*sendMiner == "" ||
+			*sendAmount <= 0 {
+
+			fmt.Println("error: invalid send arguments")
+			os.Exit(1)
+		}
+
+		cli.Send(
+			*sendFrom,
+			*sendTo,
+			*sendAmount,
+			*sendMiner,
+			*sendData,
+		)
 	}
 }
 
@@ -107,4 +137,40 @@ func (cli *CLI) GetBalance(address string) {
 	}
 
 	fmt.Printf("Balance of %s: %d\n", address, balance)
+}
+
+func (cli *CLI) Send(
+	from string,
+	to string,
+	amount int64,
+	miner string,
+	data string,
+) {
+
+	// 普通转账交易
+	tx := NewTransaction(
+		from,
+		to,
+		amount,
+		cli.bc,
+	)
+
+	if tx == nil {
+		fmt.Println("transaction create failed")
+		return
+	}
+
+	// 挖矿奖励交易(简化)
+	coinbase := NewCoinbaseTX(
+		miner,
+		data,
+	)
+
+	// 打包新区块
+	cli.bc.AddBlock([]*Transaction{
+		coinbase,
+		tx,
+	})
+
+	fmt.Println("send success")
 }
