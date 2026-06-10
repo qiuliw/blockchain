e68930d901a65e59e3e95f467060d0b426922b6c
qiuliw <a1807191473@gmail.com>
2026-06-10 23:32:26 +0800

    add: 添加少许注释

--- 5-区块链与智能合约开发/5-4-以太坊-solidity/HelloFoundry/script/Counter.s.sol
+++ 5-区块链与智能合约开发/5-4-以太坊-solidity/HelloFoundry/script/Counter.s.sol
@@ -4,16 +4,20 @@ pragma solidity ^0.8.13;
 import {Script} from "forge-std/Script.sol";
 import {Counter} from "../src/Counter.sol";
 
+// 部署智能合约
 contract CounterScript is Script {
-    Counter public counter;
+    Counter public counter; // counter 变量存储的不是数据，而是部署后合约的地址
 
-    function setUp() public {}
+    function setUp() public {} // 可选初始化函数
 
     function run() public {
-        vm.startBroadcast();
 
-        counter = new Counter();
+        // startBroadcast() 标记 后面的交易需要被发送到广播。
+        // --broadcast 表示自动将标记的交易发送到链上，还是只模拟测试
+        vm.startBroadcast(); 
 
-        vm.stopBroadcast();
+        counter = new Counter(); // new 是部署操作，不只是变量赋值
+
+        vm.stopBroadcast(); // 停止广播交易，表示脚本的交易发送阶段结束。
     }
 }
--- 5-区块链与智能合约开发/5-4-以太坊-solidity/HelloFoundry/src/Counter.sol
+++ 5-区块链与智能合约开发/5-4-以太坊-solidity/HelloFoundry/src/Counter.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+// 智能合约
 contract Counter {
+    // 当前存储在链上的计数值。
+    // `public` 可见性会自动生成一个 getter 函数。
     uint256 public number;
 
+    // 将计数器设置为指定值。
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
 
+    // 将计数器加一。
     function increment() public {
         number++;
     }
